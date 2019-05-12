f627bfcea5a39bb1ecb4038c26c7aa547e92d0c6
mohakamg <mohak.kant1amg@gmail.com>
2019-05-12 12:40:13 -0500

    Added more customization

--- FractalTree.js
+++ FractalTree.js
@@ -1,22 +1,29 @@
 let angle = Math.PI / 4;
-let divisionFactorLeft = 0.75;
-let divisionFactorRight = 0.75;
+let divisionFactorLeft = 0;
+let divisionFactorRight = 0;
 let sliderAngle, sliderTreeSizeLeft, sliderTreeSizeRight;
 let toggleAngleAnimation = false, toggleAngleAnimationBtn;
 
-let time = 0;
+let toggleDrawAnimation = false, toggleDrawAnimationBtn;
 
 function setup() {
   createCanvas(800, 400);
+  createP();
+  toggleDrawAnimationBtn = createButton("Grow Tree");
+  toggleDrawAnimationBtn.mousePressed(toggleDrawAnimationFcn);
+
   createP("Adjust Tree Rotation: ");
   sliderAngle = createSlider(0, TWO_PI, angle/2, 0.01);
+
   toggleAngleAnimationBtn = createButton("Toggle Animation Angle");
   toggleAngleAnimationBtn.mousePressed(toggleAngleAnimationFcn);
 
+
+
   createP("Adjust Left Tree Size: ");
-  sliderTreeSizeLeft = createSlider(0, 0.9, 0.5, 0.01);
+  sliderTreeSizeLeft = createSlider(0, 0.9, 0.1, 0.01);
   createP("Adjust Right Tree Size: ");
-  sliderTreeSizeRight = createSlider(0, 0.9, 0.5, 0.01);
+  sliderTreeSizeRight = createSlider(0, 0.9, 0.1, 0.01);
 
 }
 
@@ -24,14 +31,36 @@ function toggleAngleAnimationFcn(){
   toggleAngleAnimation = !toggleAngleAnimation;
 }
 
+function toggleDrawAnimationFcn(){
+  toggleDrawAnimation = true;
+  divisionFactorLeft = sliderTreeSizeLeft.value();
+  divisionFactorRight = sliderTreeSizeRight.value();
+}
+
 
 function draw() {
   background(51);
   translate(width/2, height);
 
+  if(toggleDrawAnimation){
+    if(divisionFactorLeft < 0.75){
+      divisionFactorLeft += 0.001;
+    }
+    if((divisionFactorRight < 0.75)){
+      divisionFactorRight += 0.001;
+    }
 
-  divisionFactorLeft = sliderTreeSizeLeft.value();
-  divisionFactorRight = sliderTreeSizeRight.value();
+
+    if(divisionFactorLeft >= 0.75 && divisionFactorRight >= 0.75){
+      toggleDrawAnimation = false;
+      sliderTreeSizeLeft.value(0.75);
+      sliderTreeSizeRight.value(0.75);
+    }
+  } else{
+    divisionFactorLeft = sliderTreeSizeLeft.value();
+    divisionFactorRight = sliderTreeSizeRight.value();
+  }
+
 
   stroke(color(200, 100, 100));
   branch(100);
@@ -61,7 +90,8 @@ function branch(len){
   else{
     push();
     translate(0, -len);
-    fill(255, 150);
+    noStroke();
+    fill(255, 100);
     ellipse(0, 0, 4);
     pop();
   }
